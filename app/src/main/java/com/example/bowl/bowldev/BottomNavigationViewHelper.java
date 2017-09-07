3e269ca900603345b0426a2fc28b08b2a595d193
Rabara, Reeza <rabara.reeza@gmail.com>
2017-09-07 06:51:18 -0700

    Initial commit with working bottom nav bar.

---
+++ app/src/main/java/com/example/bowl/bowldev/BottomNavigationViewHelper.java
@@ -0,0 +1,42 @@
+package com.example.bowl.bowldev;
+
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.util.Log;
+import java.lang.reflect.Field;
+
+/**
+ * Description:
+ *  BottomNavigationViewHelper removes the icon animation.
+ *
+ * Copied From:
+ *  https://stackoverflow.com/questions/41649494/how-to-remove-icon-animation-for-bottom-navigation-view-in-android
+ *
+ * @author  Reeza Rabara
+ * @version 1.0
+ * @since   2018-09-06
+ */
+class BottomNavigationViewHelper {
+    static void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                //noinspection RestrictedApi
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                //noinspection RestrictedApi
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("BNVHelper", "Unable to get shift mode field", e);
+        } catch (IllegalAccessException e) {
+            Log.e("BNVHelper", "Unable to change value of shift mode", e);
+        }
+    }
+}
