d92dd0aead15b8141c77d3ecd97ce3b983da9586
Rabara, Reeza <rabara.reeza@gmail.com>
2017-09-21 15:16:05 -0700

    added firebase

--- app/src/main/java/com/example/bowl/bowldev/FragmentProfile.java
+++ app/src/main/java/com/example/bowl/bowldev/FragmentProfile.java
@@ -1,12 +1,21 @@
 package com.example.bowl.bowldev;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class FragmentProfile extends Fragment {
+    private final String TAG = "FragmentProfile";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     public static FragmentProfile newInstance() {
         FragmentProfile fragProfile = new FragmentProfile();
         return fragProfile;
@@ -15,6 +24,22 @@ public class FragmentProfile extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
     }
 
     @Override
